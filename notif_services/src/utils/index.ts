37cf96928656c42339417e81102269e662953790
Dwi Adi Bangkit <dwi.bangkit@djembatandua.com>
2024-05-11 23:23:03 +0700

    feat(function):[all-services] add utils

--- notif_services/src/utils/index.ts
+++ notif_services/src/utils/index.ts
@@ -1,4 +1,5 @@
 import exp from "constants";
+import express, { NextFunction } from "express";
 import jwt from "jsonwebtoken";
 export const generateJwtToken = (userId: number): Promise<string> => {
   return new Promise<string>((resolve, reject) => {
@@ -15,5 +16,56 @@ export const generateJwtToken = (userId: number): Promise<string> => {
       }
       resolve(token as string);
     });
+  });
+};
+
+export const sendResponse = (
+  res: express.Response,
+  code: number,
+  success: boolean,
+  data: any,
+  errorMessage: string
+) => {
+  res.status(code).json({
+    success,
+    data,
+    error_message: errorMessage,
+  });
+};
+
+export const successResponse = (
+  res: express.Response,
+  data: any,
+  errorMessage: string
+) => {
+  res.status(200).json({
+    success: true,
+    data: data,
+    error_message: errorMessage,
+  });
+};
+
+interface ErrorStatus extends Error {
+  status: number;
+}
+
+export const errorRes = async (
+  err: ErrorStatus,
+  req: express.Request,
+  res: express.Response,
+  next: NextFunction
+) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  const errStack = err.stack;
+
+  if (status >= 500) {
+    console.error(errStack);
+  }
+
+  res.status(status).json({
+    success: false,
+    status: status,
+    message: message,
   });
 };
