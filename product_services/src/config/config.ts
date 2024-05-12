e9771b9d60c6341f33808ccf06d94af57db10fb2
Dwi Adi Bangkit <dwi.bangkit@djembatandua.com>
2024-05-13 00:08:46 +0700

    feat(function):[product-services] config env

--- product_services/src/config/config.ts
+++ product_services/src/config/config.ts
@@ -1,11 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 const config = {
   host: process.env.HOST || "localhost",
-  port: process.env.HOST_PORT || 8082,
+  port: process.env.HOST_PORT || 5004,
   db_host: process.env.DB_HOST || "localhost",
   db_port: process.env.DB_PORT || "3306",
   db_user: process.env.DB_USER || "root",
   db_password: process.env.DB_PASS || "ROOT",
-  db_name: process.env.DB_TEST || "productservicesdb",
+  db_name: process.env.DB_NAME || "productservicesdb",
   rabbitmq_host: process.env.RABBIT_HOST || "localhost",
 };
 
