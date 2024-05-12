4bb475c77406008f95c5308e3c9d06ba9f29e2c0
Dwi Adi Bangkit <dwi.bangkit@djembatandua.com>
2024-05-12 21:29:39 +0700

    feat(function):[user-services] add openapi validator

--- user_services/src/config/config.ts
+++ user_services/src/config/config.ts
@@ -4,7 +4,7 @@ const config = {
   db_host: process.env.DB_HOST || "localhost",
   db_port: process.env.DB_PORT || "3306",
   db_user: process.env.DB_USER || "root",
-  db_password: process.env.DB_PASS || "",
+  db_password: process.env.DB_PASS || "ROOT",
   db_name: process.env.DB_TEST || "userservicesdb",
   jwt_secret: process.env.SECRET_KEY || "",
 };
