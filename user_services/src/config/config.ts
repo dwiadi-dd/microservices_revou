0be40bdf6fe7070952b67166209d427bb4c7a7d9
Dwi Adi Bangkit <dwi.bangkit@djembatandua.com>
2024-05-12 22:18:14 +0700

    feat(function):[user-services] enviroment config env

--- user_services/src/config/config.ts
+++ user_services/src/config/config.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 const config = {
   host: process.env.HOST || "localhost",
   port: process.env.HOST_PORT || 5001,
@@ -5,8 +7,7 @@ const config = {
   db_port: process.env.DB_PORT || "3306",
   db_user: process.env.DB_USER || "root",
   db_password: process.env.DB_PASS || "ROOT",
-  db_name: process.env.DB_TEST || "userservicesdb",
-  jwt_secret: process.env.SECRET_KEY || "",
+  db_name: process.env.DB_NAME || "userservicesdb",
 };
 
 export default config;
