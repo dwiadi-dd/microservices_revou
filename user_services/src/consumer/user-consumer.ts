629764ddf8454a53f9a15b858c3efc0608c5b913
Dwi Adi Bangkit <dwi.bangkit@djembatandua.com>
2024-05-13 09:58:11 +0700

    feat(module):[user-services] microservice build and rabbitmq connection

--- user_services/src/consumer/user-consumer.ts
+++ user_services/src/consumer/user-consumer.ts
@@ -1,25 +1,28 @@
 import amqp from "amqplib/callback_api";
 
 function sendToQueue(msg: string) {
-  amqp.connect("amqp://localhost", function (error0, connection) {
-    if (error0) {
-      throw error0;
-    }
-    connection.createChannel(function (error1, channel) {
-      if (error1) {
-        throw error1;
+  amqp.connect(
+    `amqp://${process.env.RABBIT_HOST}`,
+    function (error0, connection) {
+      if (error0) {
+        throw error0;
       }
+      connection.createChannel(function (error1, channel) {
+        if (error1) {
+          throw error1;
+        }
 
-      const queue = "notificationQueue";
+        const queue = "notificationQueue";
 
-      channel.assertQueue(queue, {
-        durable: false,
+        channel.assertQueue(queue, {
+          durable: false,
+        });
+
+        channel.sendToQueue(queue, Buffer.from(msg));
+        console.log(" [x] Sent %s", msg);
       });
-
-      channel.sendToQueue(queue, Buffer.from(msg));
-      console.log(" [x] Sent %s", msg);
-    });
-  });
+    }
+  );
 }
 
 function onUserAction(msg: string) {
